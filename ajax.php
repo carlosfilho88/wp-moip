883e8eb86f56a3056cb05cc32c5a12124489309b
Herberth Amaral <herberthamaral@gmail.com>
2010-11-05 11:50:59 +0100

    Removendo DOCUMENT_ROOT do ajax.php e colocando o caminho relativo

--- ajax.php
+++ ajax.php
@@ -1,6 +1,6 @@
 <?php 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-config.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-includes/wp-db.php');
+require_once('../../../wp-config.php');
+require_once('../../../wp-includes/wp-db.php');
 
 	setlocale(LC_MONETARY,"pt_BR", "ptb");
 	global $wpdb;
