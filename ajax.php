e0929fc742cc315f38ad746d102f629183bcbe13
Herberth Amaral <herberthamaral@gmail.com>
2010-11-05 14:18:40 +0100

    Mais problemas de DOCUMENT_ROOT e adição da forma de pagamento na listagem de transações

--- ajax.php
+++ ajax.php
@@ -51,7 +51,7 @@ require_once('../../../wp-includes/wp-db.php');
 				<th scope="col"><?php _e("Status pagamento"); ?></th>
 				<!--<th scope="col"><?php //_e('Forma pagamento'); ?></th>-->
 				<th scope="col"><?php _e('Tipo pagamento'); ?></th>
-				<th scope="col"><?php _e('Forma de Pagamento'). ?></th>
+				<th scope="col"><?php _e('Forma de Pagamento'); ?></th>
 				<th scope="col"><?php _e('Email consumidor'); ?></th>
 				
 			  </tr>
@@ -66,7 +66,7 @@ require_once('../../../wp-includes/wp-db.php');
 					<td><?php echo(convertStatusPagamento($show->status_pagamento)); ?></td>
 					<!--<td><?php //echo($show->forma_pagamento); ?></td>-->
 					<td><?php echo(preg_replace('/([a-z0-9])([A-Z])/','$1 $2',$show->tipo_pagamento)); ?></td>
-					<td><?php echo($show->forma_pagamento) ?></td>
+					<td><?php echo(convertFormaPagamento($show->forma_pagamento)); ?></td>
 					<td><?php echo($show->email_consumidor); ?></td>
 				</tr>
 				<?php } ?>
@@ -90,6 +90,33 @@ function convertStatusPagamento($status){
 	                      "5" => "Cancelado" , 
         	              "6" => "Em An&aacute;lise");
 	return $statusArray[$status];
+}
+
+function convertFormaPagamento($status)
+{
+	$formaArray = array("1" => "Saldo MoIP pela Internet",
+			    "3" => "Visa Crédito",
+			    "4" => "Visa Débito",
+			    "5" => "Mastercad Débito",
+			    "6" => "Diners Crédito",
+			    "7" => "Amex Crédito",
+			    "8" => "BB Transferência",
+			    "9" => "BB Boleto",
+			    "10" => "BB Financiamento",
+			    "12" => "Itaú ainda não escolhido",
+			    "13" => "Itaú Transferência",
+			    "14" => "Itaú Boleto",
+			    "21" => "Bradesco Financiamento",
+			    "22" => "Bradesco Transferência",
+			    "24" => "Bradesco Boleto",
+			    "31" => "Real Financiamento",
+			    "32" => "Real Transferência",
+			    "35" => "Real Boleto",
+			    "58" => "MoIP Pelo Celular",
+			    "73" => "Boleto Bancário",
+			    "75" => "Hipercard",
+			    "76" => "Oi Paggo");
+	return $formaArray[$status];
 }
 
 ?>
--- ajax_button.php
+++ ajax_button.php
@@ -1,6 +1,6 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-config.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-includes/wp-db.php');
+require_once('../../../wp-config.php');
+require_once('../../../wp-includes/wp-db.php');
 
 ?>
 <div class="mn_ajax_reload" id="mn_ajax_reload" style="float:right;margin: 0px 30px 10px;">
